4663a9ffc4e1b0d054b7d5f148e6db00c653c039
Philippe Blayo <philippe.blayo@gmail.com>
2015-05-25 00:00:15 +0200

    Add example for java 8

---
+++ examples/java-8-with-error/src/main/java/org/gradle/Person.java
@@ -0,0 +1,13 @@
+package org.gradle;
+
+import java.util.Arrays;
+
+public class Person {
+
+    /**
+    * stream was introduced in java 8
+    */
+    public Person() {
+         Arrays.stream(new Object[] {} );
+    }
+}
